c934b2221de6ea76d291608cee85296619f90bae
Josef Ahmad <josef.ahmad@intel.com>
2015-01-21 10:47:48 +0000

    Add appended copyright to some adopt-and-modify driver
    - slcan.c
       - We added channel multiplexing over a single TTY line (aka XSLCAN)
       - This driver has no original copyright
       - Action: add a 'partial' copyright applying to XSLCAN only
    
    Signed-off-by: Josef Ahmad <josef.ahmad@intel.com>
    Signed-off-by: Dheeraj Jamwal <dheerajx.s.jamwal@intel.com>
    Change-Id: I6383c201015231d360c6a2dff2d2cfae762b2698
    Reviewed-on: https://git-gar-1.devtools.intel.com/gerrit/5120
    Reviewed-by: Tan, Raymond <raymond.tan@intel.com>
    Tested-by: Tan, Raymond <raymond.tan@intel.com>

--- drivers/net/can/slcan.c
+++ drivers/net/can/slcan.c
@@ -7,6 +7,8 @@
  *                   Fred N. van Kempen <waltje@uwalt.nl.mugnet.org>
  * slcan.c Author  : Oliver Hartkopp <socketcan@hartkopp.net>
  *
+ * SLCAN channel muxing (XSLCAN) is Copyright (C) 2015 Intel Corporation.
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 2 of the License, or (at your
