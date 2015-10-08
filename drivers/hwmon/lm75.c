cb3f1c3023a2aa7fb45ac478fd99f334e1f99156
Ben Gardner <gardner.ben@gmail.com>
2015-10-08 02:55:20 +0000

    hwmon: (lm75) Add support for TMP75C
    The TMP75C has a different control register layout and only supports
    12-bit temperature samples (0.0625 deg C).
    The continuous sample rate is ~12 Hz.
    
    Signed-off-by: Ben Gardner <gardner.ben@gmail.com>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    (cherry picked from linux-next kernel commit 9c32e815cf9e29d5cfed738ad1cb3d07ea1bb67c)
    
    Conflicts:
        Documentation/hwmon/lm75
    
    Signed-off-by: William Rafferty <william.a.rafferty@intel.com>
    Change-Id: Ia38a77f6b9098722dcdae0a3bb5bd47a62993c50

--- drivers/hwmon/lm75.c
+++ drivers/hwmon/lm75.c
@@ -55,6 +55,7 @@ enum lm75_type {		/* keep sorted in alphabetical order */
 	tmp175,
 	tmp275,
 	tmp75,
+	tmp75c,
 };
 
 /* Addresses scanned */
@@ -264,6 +265,11 @@ lm75_probe(struct i2c_client *client, const struct i2c_device_id *id)
 		data->resolution = 12;
 		data->sample_time = HZ / 2;
 		break;
+	case tmp75c:
+		clr_mask |= 1 << 5;		/* not one-shot mode */
+		data->resolution = 12;
+		data->sample_time = HZ / 4;
+		break;
 	}
 
 	/* configure as specified */
@@ -337,6 +343,7 @@ static const struct i2c_device_id lm75_ids[] = {
 	{ "tmp175", tmp175, },
 	{ "tmp275", tmp275, },
 	{ "tmp75", tmp75, },
+	{ "tmp75c", tmp75c, },
 	{ /* LIST END */ }
 };
 MODULE_DEVICE_TABLE(i2c, lm75_ids);
